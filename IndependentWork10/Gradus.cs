dc3586e27731981009cef464701fdef7e250f9c2
User <User@DESKTOP-A50L8A1>
2022-10-03 14:37:31 +1000

    Добавьте файлы проекта.

---
+++ IndependentWork10/Gradus.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork10
+{
+    internal class Circle
+    {
+        
+        double gradus;
+        double min;
+        double sec;
+        public bool isCorrect=true;
+        public double Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                gradus = value % 360;
+            }
+        }
+
+        public double Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Gradus += value / 60;
+                    min = value % 60;
+                }
+            }
+        }
+        public double Sec
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Min += value / 60;
+                    sec = value % 60;
+                }
+            }
+        }
+        public Circle(double gradus,  double min,  double sec)
+        {
+            
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+            
+        }
+        public double ToRadians() 
+        {
+            return ((sec + min * 60 + gradus * 60 * 60)/3600) * Math.PI / 180;
+        }
+
+    }
+}
---
+++ IndependentWork10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle radians;
+            {
+                do
+                {
+                    Console.WriteLine("Введите градусы, минуты, секунды");
+                    double gradus_ = Convert.ToInt32(Console.ReadLine());
+                    double min_ = Convert.ToInt32(Console.ReadLine());
+                    double sec_ = Convert.ToInt32(Console.ReadLine());
+                    radians = new Circle(gradus_, min_, sec_);
+                }
+                while (radians.isCorrect == false);
+                double totalRadians= radians.ToRadians();
+                Console.WriteLine(totalRadians);
+                Console.ReadKey();
+
+
+
+            }
+        }
+    }
+}
